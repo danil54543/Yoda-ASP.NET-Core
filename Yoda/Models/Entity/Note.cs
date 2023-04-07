32d55dc4ed440b5c57170543dbda693a307da6bf
Данил Королёв <danil54543@gmail.com>
2023-04-07 23:39:09 +0300

    Add project files.

---
+++ Yoda/Models/Entity/Note.cs
@@ -0,0 +1,16 @@
+﻿namespace Yoda.Models.Entity
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Item { get; set; }
+        public string? Marker { get; set; }
+        public bool IsConfirm { get; set; }
+        public bool IsFavorite { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateEdit { get; set; }
+        public string YodaUserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Yoda/Models/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Yoda.Models.Entity
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public List<Note>? Notes { get; set; }
+    }
+}
